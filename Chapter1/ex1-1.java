698ef4be67fa518903fffcf94b92ce18dc32c778
morgan5005 <morgan5005@users.noreply.github.com>
2018-09-21 18:52:13 -0400

    "Added chapter 1 and exercises 1-5

---
+++ Chapter1/ex1-1.java
@@ -0,0 +1,16 @@
+public class Stewie {
+	public static void main(String[] args){
+		line1();
+		line2();
+		line3();
+	}
+	public static void line1(){
+		System.out.println("//////////////////////");
+	}
+	public static void line2(){
+		System.out.println("|| Victory is mine! ||");
+	}
+	public static void line3(){
+		System.out.println("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");
+	}
+}
---
+++ Chapter1/ex1-1.txt
@@ -0,0 +1,5 @@
+Write a complete Java program in a class named Stewie that prints the following output:
+
+//////////////////////
+|| Victory is mine! ||
+\\\\\\\\\\\\\\\\\\\\\\
